24ebf0b219d65462287c32dfdd6c4b74c161da97
nareknaltakyan1 <nareknaltakyan1@gmail.com>
2024-09-02 20:11:09 +0400

    add getters and setters for BaseEntity

--- src/main/java/com/nnaltakyan/api/core/common/domain/BaseEntity.java
+++ src/main/java/com/nnaltakyan/api/core/common/domain/BaseEntity.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.Instant;
 
 @MappedSuperclass
+@Getter
+@Setter
 public abstract class BaseEntity
 {
 
